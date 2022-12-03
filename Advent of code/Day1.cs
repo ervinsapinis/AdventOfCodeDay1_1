4364333d6b6c41a1d8e7c604a73e4b6d69972fe5
ervinsapinis <larmantine@gmail.com>
2022-12-03 18:40:31 +0200

    Add project files.

---
+++ Advent of code/Day1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCodeDay1_1.Advent_of_code
+{
+    internal class Day1
+    {
+        public static void Run()
+        {
+            string[] lines = System.IO.File.ReadAllLines(@"D:\code\adventOfCode\task1.txt");
+            var sumList = new List<int>();
+            int tempSum = 0;
+
+            foreach (string line in lines)
+            {
+                if (line != "")
+                {
+                    tempSum = tempSum + int.Parse(line);
+                }
+                else
+                {
+                    sumList.Add(tempSum);
+                    tempSum = 0;
+                }
+            }
+
+            Console.WriteLine(sumList.Count);
+            Console.WriteLine("Now writing all sums");
+            foreach (int sum in sumList)
+                Console.WriteLine(sum);
+
+            Console.WriteLine();
+            Console.WriteLine(sumList.Max());
+
+            var topThree = sumList.OrderByDescending(p => p).Take(3).Sum();
+            Console.WriteLine();
+            Console.WriteLine(topThree);
+        }
+    }
+}
---
+++ Advent of code/Day2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;s
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCodeDay1_1.Advent_of_code
+{
+    internal class Day2
+    {
+        public static void Run()
+        {
+            Console.WriteLine(123);
+        }
+    }
+}
